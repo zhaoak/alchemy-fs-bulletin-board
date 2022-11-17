fbc9fb43176c262a1d2880475be1356711793d15
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-16 17:55:19 -0800

    login/sign up functionality implemented

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,26 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcWR5ZGNqbWFqZGpteXp0em92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMjAsImV4cCI6MTk4MzY4NDAyMH0.r6bSNSp-6Ts4GRV3-pnwjFMUWdUGlWU4EiIWbDqrTXU';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signUpUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+    return response.user;
+}
+
+export async function checkAuth() {}
+
+export async function redirectIfLoggedIn() {}
+
+export async function logout() {
+    await client.auth.signOut();
+    return (window.location.href = '../');
+}
