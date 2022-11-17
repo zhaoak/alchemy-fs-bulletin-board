ce4a24f7f8b8bd66988751dc7756d62cb2a6a164
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-16 16:58:21 -0800

    added supabase script fetch to html files

---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://njqdydcjmajdjmyztzov.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcWR5ZGNqbWFqZGpteXp0em92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMjAsImV4cCI6MTk4MzY4NDAyMH0.r6bSNSp-6Ts4GRV3-pnwjFMUWdUGlWU4EiIWbDqrTXU';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
